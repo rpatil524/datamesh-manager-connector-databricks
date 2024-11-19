40c134fe96efef6e95c6f8d0a07163e8251671e9
jochen <jochen.christ@innoq.com>
2024-11-19 15:18:14 +0100

    WIP

--- src/main/java/datameshmanager/databricks/Application.java
+++ src/main/java/datameshmanager/databricks/Application.java
@@ -6,7 +6,6 @@ import datameshmanager.sdk.DataMeshManagerAssetsSynchronizer;
 import datameshmanager.sdk.DataMeshManagerClient;
 import datameshmanager.sdk.DataMeshManagerEventListener;
 import datameshmanager.sdk.DataMeshManagerStateRepository;
-import datameshmanager.sdk.DataMeshManagerStateRepositoryInMemory;
 import datameshmanager.sdk.DataMeshManagerStateRepositoryRemote;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication(scanBasePackages = "datameshmanager")
 @ConfigurationPropertiesScan("datameshmanager")
@@ -45,22 +46,37 @@ public class Application {
     return new DataMeshManagerStateRepositoryRemote(client);
   }
 
-  @Bean(initMethod = "start", destroyMethod = "stop")
+  @Bean(destroyMethod = "stop")
   @ConditionalOnProperty(value = "datameshmanager.client.databricks.accessmanagement.enabled", havingValue = "true")
   public DataMeshManagerEventListener dataMeshManagerEventListener(DataMeshManagerClient client, DatabricksProperties databricksProperties,
-      WorkspaceClient workspaceClient, DataMeshManagerStateRepository stateRepository) {
+      WorkspaceClient workspaceClient, DataMeshManagerStateRepository stateRepository, TaskExecutor taskExecutor) {
     var agentid = databricksProperties.accessmanagement().agentid();
     var eventHandler = new DatabricksAccessManagementHandler(client, databricksProperties, workspaceClient);
-    return new DataMeshManagerEventListener(agentid, client, eventHandler, stateRepository);
+    var dataMeshManagerEventListener = new DataMeshManagerEventListener(agentid, client, eventHandler, stateRepository);
+    taskExecutor.execute(dataMeshManagerEventListener::start);
+    return dataMeshManagerEventListener;
   }
 
-  @Bean(initMethod = "start", destroyMethod = "stop")
+  @Bean(destroyMethod = "stop")
   @ConditionalOnProperty(value = "datameshmanager.client.databricks.assets.enabled", havingValue = "true")
   public DataMeshManagerAssetsSynchronizer dataMeshManagerAssetsSynchronizer(DatabricksProperties databricksProperties,
-      DataMeshManagerClient client, WorkspaceClient workspaceClient, DataMeshManagerStateRepository stateRepository) {
+      DataMeshManagerClient client, WorkspaceClient workspaceClient, DataMeshManagerStateRepository stateRepository, TaskExecutor taskExecutor) {
     var agentid = databricksProperties.assets().agentid();
     var assetsSupplier = new DatabricksAssetsSupplier(workspaceClient, stateRepository, databricksProperties);
-    return new DataMeshManagerAssetsSynchronizer(agentid, client, assetsSupplier);
+    var dataMeshManagerAssetsSynchronizer = new DataMeshManagerAssetsSynchronizer(agentid, client, assetsSupplier);
+    taskExecutor.execute(dataMeshManagerAssetsSynchronizer::start);
+    return dataMeshManagerAssetsSynchronizer;
+  }
+
+  @Bean
+  public TaskExecutor taskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(5);
+    executor.setMaxPoolSize(10);
+    executor.setQueueCapacity(25);
+    executor.setThreadNamePrefix("datameshmanager-agent-");
+    executor.initialize();
+    return executor;
   }
 
 }
